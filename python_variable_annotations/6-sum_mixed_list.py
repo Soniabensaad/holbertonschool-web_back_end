3a53e0e80dfa83ef844164f68d4f331ba19dc282
sonia <https://github.com/Soniabensaad>
2023-09-18 23:49:28 +0200

    python advanced

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@ and returns their sum as a float."""
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Function to sum a list of mixed integers and floats."""
     total = 0.0  # Initialize the total as a float
     for x in mxd_lst:
         total += float(x)  # Convert each element to a float and add to total
     return total
-
