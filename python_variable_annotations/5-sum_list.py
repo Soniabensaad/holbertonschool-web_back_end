befd16b686a5749f35b900b66022026932698669
sonia <https://github.com/Soniabensaad>
2023-09-18 23:35:10 +0200

    python advanced

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function sum_list which takes a list input_list of floats as argument
+and returns their sum as a float."""
+
+from typing import List
+def sum_list(input_list: List[float]) -> float:
+    """function sum_list """
+    return sum(x for x in input_list)
