c03650f973bf7771b27c2cc7af22c76c3c37186d
sonia <https://github.com/Soniabensaad>
2023-09-20 19:59:43 +0200

    python advanced

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""function make_multiplier
+that takes a float multiplier
+as argument and returns a function
+that multiplies a float by multiplier."""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Function that returns a multiplier function."""
+    
+    def multiplier_function(x: float) -> float:
+        """Inner function that multiplies x by the multiplier."""
+        return x * multiplier
+    
+    return multiplier_function
