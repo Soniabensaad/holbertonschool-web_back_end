82d20b966e3ab9d7f7a9ce17ccdec9a85157815e
sonia <https://github.com/Soniabensaad>
2023-09-20 19:13:07 +0200

    python advanced

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""" function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
+""" function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple.
 The first element of the tuple is the string k"""
 
 
 from typing import Tuple, Union
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function to_kv"""
     return (k, v * v)
