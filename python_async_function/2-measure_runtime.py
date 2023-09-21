9304300e5f1b7cf4f080d1779356e12e4c8b203d
sonia <https://github.com/Soniabensaad>
2023-09-21 18:08:29 +0200

    python async

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,11 +4,13 @@ import random
 import time
 from typing import List
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine"""
     delay = random.uniform(0, float(max_delay))
     await asyncio.sleep(delay)
     return delay
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrency."""
@@ -16,11 +18,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     results = await asyncio.gather(*tasks)
     return sorted(results)
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the runtime"""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-
     end_time = time.time()
     elapsed_time = end_time - start_time
-    return elapsed_time / n  
+    return elapsed_time / n
