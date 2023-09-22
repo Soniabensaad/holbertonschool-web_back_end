980a65713992bc9c69ec382cab257d749464a9d2
sonia <https://github.com/Soniabensaad>
2023-09-22 21:21:07 +0200

    python async

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,11 +7,13 @@ import asyncio
 import random
 from typing import List
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine"""
     delay = random.uniform(0, float(max_delay))
     await asyncio.sleep(delay)
     return delay
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrency."""
@@ -19,7 +21,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     results = await asyncio.gather(*tasks)
     return sorted(results)
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """The code is nearly identical to wait_n except
+    task_wait_random is being called."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
     return sorted(results)
