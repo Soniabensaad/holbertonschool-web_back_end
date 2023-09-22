0935de45df939c8641317115a6a441d065babfa9
sonia <https://github.com/Soniabensaad>
2023-09-22 21:51:43 +0200

    python async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,9 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
+    """coroutine called async_generator that takes no arguments."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
