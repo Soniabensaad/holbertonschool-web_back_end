cdedde94c29bb29fd79f3915beb4f8567b846204
sonia <https://github.com/Soniabensaad>
2023-09-23 15:38:47 +0200

    python async

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
-
 import asyncio
+from typing import List
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
+async def measure_runtime() -> List[float]:
     tasks = [async_comprehension() for _ in range(4)]
     results = await asyncio.gather(*tasks)
     return results
