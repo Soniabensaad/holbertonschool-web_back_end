635966c584e94dc1838c2e003a7a5fa743515aea
sonia <https://github.com/Soniabensaad>
2023-09-24 17:44:14 +0200

    caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,16 +2,16 @@
 """ Basic dictionary"""
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """BasicCache that inherits from BaseCaching"""
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
         if key is not None and item is not None:
-
-             self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """return the value in self.cache_data linked to key."""
         if key in self.cache_data:
-             return self.cache_data[key]
-        return None  
+            return self.cache_data[key]
+        return None
