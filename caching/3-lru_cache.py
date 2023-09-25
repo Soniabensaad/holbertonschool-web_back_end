fe8d2cc9e7adca2e26432348b958e04f027b9af4
sonia <https://github.com/Soniabensaad>
2023-09-25 17:28:29 +0200

    caching

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -4,22 +4,20 @@ from collections import OrderedDict
 from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
-    """ class LRUCache that inherits from
-    BaseCaching and is a caching system:"""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
-    
+
     def put(self, key, item):
-        """Must assign to the dictionary """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                self.cache_data.popitem(last=False)
+                discarded_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {discarded_key}")
             self.cache_data[key] = item
-        print(f"DISCARD: {key}")
 
     def get(self, key):
-        """Must return the value in self.cache_data"""
-        if key in self.cache_data:
-            return self.cache_data[key]
+        if key is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+                return self.cache_data[key]
         return None
