28d4ac215c3d2ffeee7c876387690b10a347606a
sonia <https://github.com/Soniabensaad>
2023-09-25 15:27:41 +0200

    caching

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ LIFO Caching"""
 from base_caching import BaseCaching
+
+
 class LIFOCache(BaseCaching):
     """class LIFOCache that inherits from BaseCaching
     and is a caching system"""
     def __init__(self):
         super().__init__()
         self.lifo = []
-    
     def put(self, key, item):
         """Must assign to the dictionary """
         if key is not None and item is not None:
@@ -17,7 +18,6 @@ class LIFOCache(BaseCaching):
                 print(f"DISCARD: {first_key}")
             self.lifo.append(key)
             self.cache_data[key] = item
-
     
     def get(self, key):
         """Must return the value in self.cache_data"""
