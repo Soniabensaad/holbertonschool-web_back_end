f760f353686f5c4dbcfffa42ab37e8d21c7b23d4
sonia <https://github.com/Soniabensaad>
2023-09-26 20:07:11 +0200

    pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,39 @@
+import csv
+import math
+from typing import List, Tuple
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        
+        if page < 1 or page > total_pages:
+            return []
+        
+        start, end = self.index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        total_rows = len(self.dataset())
+        total_pages = math.ceil(total_rows / page_size)
+        
+        start = (page - 1) * page_size
+        end = min(start + page_size, total_rows)
+        
+        return start, end
