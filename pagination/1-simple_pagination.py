393bd1134d0c9b7aa906c003bbf31e398158aa7c
sonia <https://github.com/Soniabensaad>
2023-09-26 20:13:06 +0200

    pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,6 +3,7 @@ import csv
 import math
 from typing import List, Tuple
 
+
 class Server:
     """server"""
     DATA_FILE = "Popular_Baby_Names.csv"
@@ -23,13 +24,10 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """method named get_page"""
         assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        
+        assert isinstance(page_size, int) and page_size > 0 
         total_pages = math.ceil(len(self.dataset()) / page_size)
-        
         if page < 1 or page > total_pages:
             return []
-        
         start, end = self.index_range(page, page_size)
         return self.dataset()[start:end]
 
@@ -38,8 +36,6 @@ class Server:
         correctly and return the appropriate page of the dataset"""
         total_rows = len(self.dataset())
         total_pages = math.ceil(total_rows / page_size)
-        
         start = (page - 1) * page_size
         end = min(start + page_size, total_rows)
-        
         return start, end
