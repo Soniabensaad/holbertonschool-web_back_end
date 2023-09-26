535b33611b1288ba27cd2e317fad6f3bf836b0b9
sonia <https://github.com/Soniabensaad>
2023-09-26 19:00:27 +0200

    pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,4 +2,4 @@
 """0. Simple helper function"""
 def  index_range(page, page_size):
     """should return a tuple of size"""
-    return (page - 1, page_size)
+    return ((page - 1)* page_size, (page - 1)* page_size + page_size)
