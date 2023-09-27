ff86988d1b1cc8020148a0e3b1c9cc451ff2ac64
sonia <https://github.com/Soniabensaad>
2023-09-27 23:29:33 +0200

    pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -33,6 +33,7 @@ class Server:
         return self.dataset()[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Implement a get_hyper method t"""
         items = len(self.dataset())  # Calculate the total number of items
         data = self.get_page(page, page_size)
         total_pages = math.ceil(items / page_size)
@@ -40,12 +41,12 @@ class Server:
         next_page = page + 1 if page  + 1 < page_size - 1 else None
         prev_page = page - 1 if page - 1 > 0 else None
         return {
-        "page_size": page_size,
-        "page": page,
-        "data": data,
-        "next_page": next_page,
-        "prev_page": prev_page,
-        "total_pages": total_pages
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
     }
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
