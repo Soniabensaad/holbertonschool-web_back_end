9126e782d8e7dd0e76c148aab6a40b166a7579a9
sonia <https://github.com/Soniabensaad>
2023-09-27 23:27:12 +0200

    pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -37,21 +37,16 @@ class Server:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(items / page_size)
         page_size = page_size if page < total_pages else 0
-
-    # Calculate next_page and prev_page based on page number validity
         next_page = page + 1 if page  + 1 < page_size - 1 else None
         prev_page = page - 1 if page - 1 > 0 else None
-
         return {
         "page_size": page_size,
         "page": page,
         "data": data,
-        "next_page": next_page ,
-        "prev_page": prev_page ,
+        "next_page": next_page,
+        "prev_page": prev_page,
         "total_pages": total_pages
     }
-
-
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """ find the correct indexes to paginate the dataset
