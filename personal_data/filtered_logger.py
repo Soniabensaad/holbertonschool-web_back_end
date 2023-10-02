53e75f2db9c606bc4f8cba58cb40e777de885dd1
sonia <https://github.com/Soniabensaad>
2023-10-02 20:45:00 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 import logging
 
-
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
@@ -45,3 +45,13 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
+    
+    def get_logger() -> logging.Logger:
+        """Create logger"""
+        log = logging.getLogger("user_data")
+        log.setLevel(logging.INFO)
+        log.propagate = False
+        sh = logging.StreamHandler()
+        sh.setFormatter(RedactingFormatter(PII_FIELDS))
+        log.addHandler(sh)
+        return log
