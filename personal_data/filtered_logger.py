1a8a6f7e1abadfe7f319b4eac4536f4f311ea17e
sonia <https://github.com/Soniabensaad>
2023-10-02 18:01:01 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,6 @@
 import re
 from typing import List
 
-
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
@@ -13,13 +12,12 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         fields (List[str]): List of strings representing fields to obfuscate.
         redaction (str): String representing the redaction value.
         message (str): String representing the log message.
-        separator (str): String representing the
-        character separating fields in the log message.
+        separator (str): String representing the character separating fields in the log message.
 
     Returns:
         str: The log message with specified fields obfuscated.
     """
     for f in fields:
         return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
-                  lambda match: match.group(1) + "=" + redaction
-                  if match.group(1) in fields else match.group(0), message)
+                      lambda match: match.group(1) + "=" + redaction
+                                    if match.group(1) in fields else match.group(0), message)
