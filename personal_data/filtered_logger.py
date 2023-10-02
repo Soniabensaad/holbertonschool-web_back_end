9a444a7a76d7d52ca578be8a73171ece5b96eede
sonia <https://github.com/Soniabensaad>
2023-10-02 17:44:39 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
+"""Regex-ing"""
 import re
-from typing import List, Tuple
-""" Regex-ing"""
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
-    """Write a function called filter_datum that
-    returns the log message obfuscated"""
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscate specified fields in a log message.
+
+    Args:
+        fields (List[str]): List of strings representing fields to obfuscate.
+        redaction (str): String representing the redaction value.
+        message (str): String representing the log message.
+        separator (str): String representing the character separating fields in the log message.
+
+    Returns:
+        str: The log message with specified fields obfuscated.
+    """
     for f in fields:
-        return (re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)", lambda match: match.group(1) + "=" + redaction
-                       if match.group(1) in fields else match.group(0), message)
-)
+        return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
+                      lambda match: match.group(1) + "=" + redaction
+                                    if match.group(1) in fields else match.group(0),
+                      message)
