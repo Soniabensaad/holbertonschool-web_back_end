04ca550e2b0b0666a7a0edfd1093d7945f9fa7f6
sonia <https://github.com/Soniabensaad>
2023-10-03 19:00:03 +0200

    personal data

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,7 +2,7 @@
 import bcrypt
 
 """ Encrypting passwords"""
-def hash_password(password):
+def hash_password(password: str = "") -> bytes:
     """expects one string argument name password"""
     
     password_bytes = password.encode('utf-8')
