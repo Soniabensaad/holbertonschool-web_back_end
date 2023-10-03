5ef70ac691dc88579e4c467be1a78b9a57dec6d0
sonia <https://github.com/Soniabensaad>
2023-10-03 19:02:37 +0200

    personal data

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+""" Encrypting passwords"""
 import bcrypt
 
 """ Encrypting passwords"""
+
+
 def hash_password(password: str = "") -> bytes:
     """expects one string argument name password"""
-    
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    
-    
     hashed_password = bcrypt.hashpw(password_bytes, salt)
-    
     return hashed_password
