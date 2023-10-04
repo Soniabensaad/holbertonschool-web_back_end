6f0c699eb5fc3d64fc04db2cadde149ec0db0020
sonia <https://github.com/Soniabensaad>
2023-10-04 18:17:29 +0200

    basic authentification

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -19,7 +19,6 @@ if AUTH_TYPE == "auth":
     auth = Auth()
 
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -38,6 +37,7 @@ def forbidden(error) -> str:
     """Error handler: Forbidden"""
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.before_request
 def before_request() -> str:
     """Request validation!"""
@@ -54,6 +54,7 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
