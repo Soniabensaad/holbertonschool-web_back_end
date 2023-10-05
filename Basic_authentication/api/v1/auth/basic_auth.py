9e29b67a37a980b4b954dc0a44b0fde0f33964fe
sonia <https://github.com/Soniabensaad>
2023-10-05 22:59:22 +0200

    basic authentification

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 """Basic auth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+
 
 
 class BasicAuth(Auth):
-    """Basic Auth"""
-    pass
+    """ Basic Auth class """
+
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+            Extract header in base64
+
+            Args:
+                authorization_header: string in base64
+
+            Return:
+                Header in base64 or None
+        """
+        if authorization_header is None\
+           or type(authorization_header) != str\
+           or not authorization_header.startswith('Basic ')\
+           and not authorization_header.endswith(' '):
+
+            return None
+
+        return authorization_header.split(' ')[1]
