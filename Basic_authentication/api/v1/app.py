df105ec7cc7643bc9546b9b3cd0b3e7505dac0c8
sonia <https://github.com/Soniabensaad>
2023-10-05 21:21:24 +0200

    basic authentification

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -45,8 +45,8 @@ def before_request() -> str:
         return
     list = [
         '/api/v1/status/',
-          '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
     ]
     if not(auth.require_auth(request.path, list)):
         return
