7f3920cdef4e44b44a2afe1791cf9a2c29a0f8bb
sonia <https://github.com/Soniabensaad>
2023-10-05 22:15:07 +0200

    basic authentification

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -43,20 +43,25 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """Request validation!"""
+    """ Before Request Handler
+    Requests Validation
+    """
     if auth is None:
         return
-    list = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-    if not(auth.require_auth(request.path, list)):
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
         return
+
     if auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
+
 
 
 if __name__ == "__main__":
