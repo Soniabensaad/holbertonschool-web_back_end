2191ab6b10edff53a434f8f0f5449cdb7f70bd78
sonia <https://github.com/Soniabensaad>
2023-10-06 19:48:24 +0200

    Session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -16,7 +16,7 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ User ID for Session ID"""
         if session_id is None or type(session_id) != str:
