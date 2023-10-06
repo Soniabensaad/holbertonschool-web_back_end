fd8748ab60a579ec5a637d8538966cb5ecbf1492
sonia <https://github.com/Soniabensaad>
2023-10-06 19:15:47 +0200

    Session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,17 @@
 """Empty session"""
 from api.v1.auth.auth import Auth
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ class SessionAuth that inherits from Auth"""
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """Create an instance method """
+        if user_id is None\
+            or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
