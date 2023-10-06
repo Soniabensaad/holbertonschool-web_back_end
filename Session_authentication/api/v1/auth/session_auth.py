c08c5ebc74e00c7caf6c6a7b285bf2fc9e7daa77
sonia <https://github.com/Soniabensaad>
2023-10-06 19:17:30 +0200

    Session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,10 +8,10 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """ class SessionAuth that inherits from Auth"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """Create an instance method """
-        if user_id is None\
-            or type(user_id) != str:
+        if user_id is None or type(user_id) != str:
             return None
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
