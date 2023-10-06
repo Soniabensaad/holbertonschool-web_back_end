12f004e1d5e31e7aeacb2eeb32ab47ee86c91941
sonia <https://github.com/Soniabensaad>
2023-10-06 22:25:19 +0200

    basic authentification

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -45,3 +45,13 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout"""
+    from api.v1.app import auth
+    destroy_session = auth.destroy_session(request)
+    if not destroy_session:
+        abort(404)
+    return jsonify({}), 200
