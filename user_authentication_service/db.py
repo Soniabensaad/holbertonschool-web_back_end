f4902b9df13a479b6d817a6ac02cc6c4694782f3
sonia <https://github.com/Soniabensaad>
2023-10-10 21:21:28 +0200

    user authentification

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -57,9 +57,8 @@ class DB:
             user = self.find_user_by(id=user_id)
 
             for key, value in kwargs.items():
-               if hasattr(user, key):
-                   setattr(user, key, value)
+                if hasattr(user, key):
+                    setattr(user, key, value)
         except ValueError:
-           print("Error")
+            print("Error")
         self._session.commit()
-   
