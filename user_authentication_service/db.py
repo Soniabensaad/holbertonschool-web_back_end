b1770d5d19caf48f2b8feeb073be5ec60441af97
sonia <https://github.com/Soniabensaad>
2023-10-10 19:56:58 +0200

    db

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -2,14 +2,10 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
-from user import User
-
-from user import Base
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
@@ -19,13 +15,13 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         """Memoized session object
         """
         if self.__session is None:
@@ -34,19 +30,23 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to database
         """
-        user = User(email=email, hashed_password=hashed_password)
+            add_user: returns a user object
+            and save the user to the database
+        """
+
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
         self._session.add(user)
         self._session.commit()
         return user
- 
+
     def find_user_by(self, **kwargs) -> User:
-        """Find user"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-        except TypeError:
+        """User finder method"""
+        if kwargs is None:
             raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
         return user
