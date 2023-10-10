9ea4e7e9b3150b2dea5a76db1a2d1c5504ec90e9
sonia <https://github.com/Soniabensaad>
2023-10-10 22:13:04 +0200

    user authentification

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Hash password"""
+
+import bcrypt
+def _hash_password(password: str) -> bytes:
+    """you will define a _hash_password"""
+    in_bytes_password = password.encode('utf-8')
+
+    #generate salt to hash password
+    salt = bcrypt.gensalt()
+    password_hashed = bcrypt.hashpw(in_bytes_password, salt)
+    return password_hashed
