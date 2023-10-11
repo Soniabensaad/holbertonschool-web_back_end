53eca40c7a16dada930d84fedf8175c6fbfa9867
sonia <https://github.com/Soniabensaad>
2023-10-11 21:41:34 +0200

    db

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -18,9 +18,11 @@ def _hash_password(password: str) -> bytes:
     password_hashed = bcrypt.hashpw(in_bytes_password, salt)
     return password_hashed
 
+
 def _generate_uuid() -> str:
     """ Generate UUIDs"""
     return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -51,5 +53,3 @@ class Auth:
 
         check = bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
         return check
-    
-    
