fa54217db580a6ad26301a64b8c0a6ace3c1320b
sonia <https://github.com/Soniabensaad>
2023-10-11 17:34:41 +0200

    db

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 
 
 import bcrypt
-
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound 
 
 def _hash_password(password: str) -> bytes:
     """you will define a _hash_password"""
@@ -13,3 +15,25 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     password_hashed = bcrypt.hashpw(in_bytes_password, salt)
     return password_hashed
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register user"""
+        try:
+            find = self._db.find_user_by(email=email)
+            if find:
+                raise ValueError(f"User <{find.email}> already exists")
+        except NoResultFound:
+            pass
+        else:
+            password_hash = _hash_password(password)
+            user = self._db.add_user(email, password_hash)
+            return user
+
+        return None
