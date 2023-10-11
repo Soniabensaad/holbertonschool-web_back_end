035bae0b450376c6b0b26a08849a6c97f665a880
sonia <https://github.com/Soniabensaad>
2023-10-11 20:22:09 +0200

    db

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -17,20 +17,20 @@ def hello():
     return jsonify(data)
 
 @app.route("/users", methods=["POST"])
-def register_user() -> str:
+def register_user():
     try:
-        
-        email = request.form["email"]
-        password = request.form["password"]
+        email = request.form.get("email")
+        password = request.form.get("password")
     except KeyError:
         abort(400)
 
     try:
         new_user = AUTH.register_user(email, password)
+        msg = {"email": new_user.email, "message": "user created"}
+        return jsonify(msg)
     except ValueError as e:
-        return jsonify({"message": "Email already registered"}), 400
+        msg = {"message": "email already registered"}
+        return jsonify(msg), 400
 
-    msg = {"email": new_user.email, "message": "User created"}
-    return jsonify(msg)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
