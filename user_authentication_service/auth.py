41ffd954dc63d5144e0a493706510fad1280bb1c
sonia <https://github.com/Soniabensaad>
2023-10-13 00:35:48 +0200

    app

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,6 +67,9 @@ class Auth:
     def get_user_from_session_id(self, session_id:str) ->str:
         """Find user by session ID"""
         if not session_id:
-            return 
-        user = self._db.find_user_by(session_id=session_id)
-        return user.email
+            return
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return
