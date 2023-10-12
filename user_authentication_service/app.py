b600b094b817b1260626cee8e9c4c96434677778
sonia <https://github.com/Soniabensaad>
2023-10-12 23:46:17 +0200

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -35,16 +35,16 @@ def register_user():
 
 @app.route("/sessions", methods=["POST"])
 def login() -> str:
-    try:
-        email = request.form.get("email")
-        password = request.form.get("password")
-    except KeyError:
-        abort(401)
+    email = request.form.get("email")
+    password = request.form.get("password")
+   
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in", "session_id": session_id})
         response.set_cookie("session_id", session_id)
         return response
+    else:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
