08de23e9d33ff13cf668cceb0aa5202524073b73
sonia <https://github.com/Soniabensaad>
2023-10-13 22:08:17 +0200

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -87,14 +87,19 @@ def get_reset_password_token() -> str:
 
 @app.route('/reset_password', methods=["PUT"])
 def update_password() -> str:
-    email = request.form.get("email")
-    password = request.form.get("password")
-    reset_token = request.form.get("reset_token")
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        password = request.form.get('new_password')
+    except KeyError:
+        abort(403)
+
     try:
         AUTH.update_password(reset_token, password)
-        return jsonify({"email": f"{email}", "message": "Password updated"}), 200
-    except Exception:
+    except ValueError:
         abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
