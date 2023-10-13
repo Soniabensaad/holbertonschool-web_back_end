d3d67960ac88bc49540d9bf3ef6f34da4f90be08
sonia <https://github.com/Soniabensaad>
2023-10-13 20:51:58 +0200

    app

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -84,8 +84,7 @@ class Auth:
         try:
            user = self._db.find_user_by(email=email)
            reset_token = _generate_uuid()
-           update = self._db.update_user(user.id, reset_token=reset_token)
-           return update
-
-        except ValueError:
-            return
+           self._db.update_user(user.id, reset_token=reset_token)
+           return reset_token
+        except NoResultFound:
+            raise ValueError
