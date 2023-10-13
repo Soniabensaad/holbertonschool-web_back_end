abec1b419758c8d529377c878bf1c7fc788f7c9a
sonia <https://github.com/Soniabensaad>
2023-10-13 19:57:14 +0200

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -50,6 +50,17 @@ def login() -> str:
     else:
         abort(401)
 
+@app.route("sessions", methods=["DELETE"])
+def logout() -> str:
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        response.set_cookie("session_id", session_id)
+        session_id.clear()
+        return redirect(url_for('GET/'))
+    else:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
