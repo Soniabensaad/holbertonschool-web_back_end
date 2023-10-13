f498b76d42f10e58eae49f8a8821b98d468d9c58
sonia <https://github.com/Soniabensaad>
2023-10-13 21:41:12 +0200

    app

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -88,3 +88,22 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+        
+     def update_password(self, reset_token: str, password: str) -> None:
+        """ Update Password
+        """
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except (NoResultFound):
+            raise ValueError
+
+        new_passwd = _hash_password(password)
+
+        self._db.update_user(
+            (user.id), hashed_password=new_passwd,
+            reset_token=None)
+
+        return None
