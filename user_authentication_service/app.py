e47d9193cfc3ef319af0c3f286a38b77dec42188
sonia <https://github.com/Soniabensaad>
2023-10-13 20:20:36 +0200

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -50,21 +50,18 @@ def login() -> str:
     else:
         abort(401)
 
-@app.route("sessions", methods=["DELETE"])
+@app.route('/sessions', methods=['DELETE'])
 def logout() -> str:
+    """ logout
+       str: message
+    """
     session_id = request.cookies.get('session_id')
-
-    if session_id is None:
-        abort(403)
-
     user = AUTH.get_user_from_session_id(session_id)
-
-    if user is None:
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
         abort(403)
-
-    AUTH.destroy_session(user.id)
-
-    return redirect('/')
 
 
 if __name__ == "__main__":
