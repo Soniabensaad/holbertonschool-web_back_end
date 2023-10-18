460f5e996ac4c3b0a814b4586c89cea2a93a5d4b
sonia <https://github.com/Soniabensaad>
2023-10-18 22:01:12 +0200

    unit test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,19 +5,18 @@ from unittest.mock import patch
 from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized, parameterized_class
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """ unit test"""
     @parameterized.expand([
-    ({"a": 1}, ("a",), 1),
-    ({"a": {"b": 2}}, ("a",), {"b": 2}),
-    ({"a": {"b": 2}}, ("a", "b"), 2)
-])
-
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
 
     def test_access_nested_map(self, nested_map, path, result):
         """test_access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), result)
-
 
     @parameterized.expand([
     ({}, ("a",), "Key 'a' not found in the nested_map"),
