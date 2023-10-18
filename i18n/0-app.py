ea3883c1cf7086698b658eb741346865971580f4
sonia <https://github.com/Soniabensaad>
2023-10-18 23:25:59 +0200

    unit test

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,13 +2,21 @@
 """0. Basic Flask app
 """
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from os import getenv
 
-app = Flask(__name)
+app = Flask(__name__, static_url_path='')
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world():
-    return render_template('0-ondex.html')
+def index():
+    """ GET /
+        Return: 0-index.html
+    """
+    return render_template('0-index.html')
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
