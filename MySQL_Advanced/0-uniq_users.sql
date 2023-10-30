dbabcd094e704df69993506e9495e92bc44cf2ac
sonia <https://github.com/Soniabensaad>
2023-10-30 22:00:28 +0100

    mysql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- create table
 create table IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
 )
