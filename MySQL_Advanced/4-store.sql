c83669863e83612b05e4481b49c23f2122cdd2c5
sonia <https://github.com/Soniabensaad>
2023-10-31 17:43:08 +0100

    redis

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- 4. Buy buy buy
+CREATE TRIGGER DecreaseItemQuantityAfterOrder
+AFTER INSERT ON orders 
+FOR EACH ROW
+BEGIN
+    -- Decrease the stock_quantity in the items table
+    UPDATE items
+    SET stock_quantity = stock_quantity - NEW.quantity
+    WHERE item_id = NEW.item_id;
+END;
+   
