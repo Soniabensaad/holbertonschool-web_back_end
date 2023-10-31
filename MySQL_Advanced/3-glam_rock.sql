73998a220ae26c565281f4ce7d66469e661c060d
sonia <https://github.com/Soniabensaad>
2023-10-31 17:24:28 +0100

    redis

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- 3. Old school band
 SELECT band_name, (formed - split) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE main_style = 'Glam rock' AND lifespan > 0
 ORDER BY lifespan DESC;
