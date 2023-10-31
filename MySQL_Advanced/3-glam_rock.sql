817384320d327b77264248a98091a938cd042289
sonia <https://github.com/Soniabensaad>
2023-10-31 17:33:27 +0100

    redis

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- 3. Old school band
-SELECT band_name, IFNULL(split, 2023) - IFNULL(formed, 0) AS lifespan 
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%';
