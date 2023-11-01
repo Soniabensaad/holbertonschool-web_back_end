80179da2cc905f0794f5a9e1305918085f3e7f6f
sonia <https://github.com/Soniabensaad>
2023-11-01 21:47:08 +0100

    redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional
+from functools import wraps
 
 """0. Writing strings to Redis"""
 class Cache:
@@ -36,25 +37,32 @@ class Cache:
 
         return value
     
-    def __init__(self):
-        self.cache = {}
+    
+def count_calls(method: Callable = None) -> Callable:
+    """ Decorator count calls """
+    name = method.__qualname__
 
-    def store(self, key, value):
-        self.cache[key] = value
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper method """
+        self._redis.incr(name)
+        return method(self, *args, **kwargs)
 
-    def retrieve(self, key):
-        return self.cache.get(key, None)
+    return wrapper
 
-    def count_calls(method):
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            # Use the qualified name of the method as the key
-            key = method.__qualname__
 
-            # Increment the call count
-            self.store(key, self.retrieve(key, 0) + 1)
+def call_history(method: Callable) -> Callable:
+    """ Decorator call history """
 
-            # Call the original method and return its result
-            return method(self, *args, **kwargs)
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wraper function """
+        input: str = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
 
-        return wrapper
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+
+    return wrapper
