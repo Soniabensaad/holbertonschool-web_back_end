547595480a00db959bb5c95d76b40b42dee90b38
sonia <https://github.com/Soniabensaad>
2023-11-03 20:41:40 +0100

    redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-
+"""Tasks"""
 import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional
 from functools import wraps
 
-"""0. Writing strings to Redis"""
 class Cache:
-    """redis"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -16,8 +14,8 @@ class Cache:
         random = str(uuid4())
         self._redis.set(random, data)
         return random
-    
-    def get(self, key: str, fn: Callable=None) -> Union[str, bytes, int, float]:
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
         find = self._redis.get(key)
         if find is not None:
             if fn is not None:
@@ -25,40 +23,33 @@ class Cache:
             else:
                 return find
         return None
+
     def get_str(self, key: str) -> str:
         return self._redis.get(key).decode("utf-8")
-    
+
     def get_int(self, key: str) -> int:
         value = self._redis.get(key)
         try:
             value = int(value.decode('utf-8'))
         except Exception:
             value = 0
-
         return value
-    
-    
+
 def count_calls(method: Callable = None) -> Callable:
-    """ Decorator count calls """
     name = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ Wrapper method """
         self._redis.incr(name)
         return method(self, *args, **kwargs)
 
     return wrapper
 
-
 def call_history(method: Callable) -> Callable:
-    """ Decorator call history """
-
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ Wraper function """
-        input: str = str(args)
-        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        input_str = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input_str)
 
         output = str(method(self, *args, **kwargs))
         self._redis.rpush(method.__qualname__ + ":outputs", output)
@@ -67,31 +58,13 @@ def call_history(method: Callable) -> Callable:
 
     return wrapper
 
+def replay(self, method: Callable):
+    method_name = method.__qualname__
 
-def replay(func: Callable):
-    """ Replay function """
-    r = redis.Redis()
-    func_name = func.__qualname__
-    number_calls = r.get(func_name)
+    inputs = self._redis.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = self._redis.lrange(f"{method_name}:outputs", 0, -1)
 
-    try:
-        number_calls = number_calls.decode('utf-8')
-    except Exception:
-        number_calls = 0
-
-    print(f'{func_name} was called {number_calls} times:')
-
-    ins = r.lrange(func_name + ":inputs", 0, -1)
-    outs = r.lrange(func_name + ":outputs", 0, -1)
-
-    for cin, cout in zip(ins, outs):
-        try:
-            cin = cin.decode('utf-8')
-        except Exception:
-            cin = ""
-        try:
-            cout = cout.decode('utf-8')
-        except Exception:
-            cout = ""
-
-        print(f'{func_name}(*{cin}) -> {cout}')
+    print(f"{method_name} was called {len(inputs)} times:")
+    for input_str, output in zip(inputs, outputs):
+        input_args = eval(input_str)  
+        print(f"{method_name}({input_args}) -> {output}")
