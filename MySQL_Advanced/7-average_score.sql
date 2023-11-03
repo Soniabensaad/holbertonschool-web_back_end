5b03bba106b6bde924b38e615b1f5f18e5848cf8
sonia <https://github.com/Soniabensaad>
2023-11-03 21:24:49 +0100

    sql

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,9 @@
 -- 7. Average score
-SELECT user_id, AVG(score) AS average_score
-FROM user_scores
-WHERE user_id = <user_id>
-GROUP BY user_id;
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS s WHERE s.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
