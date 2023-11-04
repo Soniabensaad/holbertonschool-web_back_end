471d0534ea29eb700a2b38758f23cb2b1f26a572
sonia <https://github.com/Soniabensaad>
2023-11-04 21:57:06 +0100

    redis

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
---9. Optimize search and score
+-- Index
 CREATE INDEX idx_name_first_score
-ON names (name(1), score);CREATE INDEX idx_name_first_score ON names ( name(1), score );
+ON names (name(1), score);
