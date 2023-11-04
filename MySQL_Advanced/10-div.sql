9ba01a47ea856abd31cc6b5ba9cf7aa7ede292c3
sonia <https://github.com/Soniabensaad>
2023-11-04 21:53:29 +0100

    redis

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,11 +1,15 @@
 --10. Safe divide
-DELIMITER //
-
 DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-	RETURN (IF (b = 0, 0, a / b));
-END //
-
-DELIMITER;
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END
+//
+DELIMITER ;
