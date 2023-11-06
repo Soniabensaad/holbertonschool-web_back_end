f8299d60cee9b818cbd3f34f7d939992a90dca8b
sonia <https://github.com/Soniabensaad>
2023-11-06 22:27:28 +0100

    nosql

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,4 +4,4 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """ Insert a document in Python"""
     new = mongo_collection.insert_one(kwargs)
-    return (new.insert_id)
+    return (new.inserted_id)
