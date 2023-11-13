6d6e8f740a77d30ae4c18c762e9c4e0190bd43f7
sonia <https://github.com/Soniabensaad>
2023-11-13 13:08:55 +0100

    ES promise

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,13 @@
 // Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        if (true) {
-            resolve(); // Corrected typo here
-        } else {
-            reject();
-        }
-    });
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+    if (true) {
+      resolve(); // Corrected typo here
+    } else {
+      reject();
+    }
+    /* eslint-disable */
+  });
 }
-
 export default getResponseFromAPI();
