3fc1fc970868b36bdbead4f50839c39c0d84aaf2
sonia <https://github.com/Soniabensaad>
2023-11-14 18:09:34 +0100

    ES promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,24 +1,19 @@
 // 3-all.js
 
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const photoResponse = values[0];
-      const userResponse = values[1];
+      const { body } = values[0];
+      const { firstName, lastName } = values[1];
 
-      if (photoResponse.status === 200 && userResponse.status === 200) {
-        console.log(`Body: ${photoResponse.body}`);
-        console.log(`First Name: ${userResponse.firstName}`);
-        console.log(`Last Name: ${userResponse.lastName}`);
-      } else {
-        throw new Error('Signup system offline');
-      }
+      console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch((error) => {
-      console.error(error.message);
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
 
 export default handleProfileSignup;
+
