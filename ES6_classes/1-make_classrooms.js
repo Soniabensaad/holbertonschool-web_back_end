acf7e53f4efac1bfaf51bb358752a79b3f9684c4
sonia <https://github.com/Soniabensaad>
2023-11-15 10:44:47 +0100

    ES promise

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,7 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-  return rooms;
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
 
 export default initializeRooms;
