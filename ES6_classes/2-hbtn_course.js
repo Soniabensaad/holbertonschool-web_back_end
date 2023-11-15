c438edccda41bafb670cc4ea600bf8d90f51df09
sonia <https://github.com/Soniabensaad>
2023-11-15 16:38:46 +0100

    ES promise

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,40 +1,48 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+class HolbertonCourse {
+    constructor(name, length, students) {
+      this.name = name;
+      this.length = length;
+      this.students = students;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(name) {
+      if (typeof name === 'string') {
+        this._name = name;
+      } else {
+        throw new TypeError('Name must be a string');
+      }
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set length(length) {
+      if (typeof length === 'number') {
+        this._length = length;
+      } else {
+        throw new TypeError('Length must be a number');
+      }
+    }
+  
+    get students() {
+      return this._students;
+    }
+  
+    set students(students) {
+      if (
+        Array.isArray(students)
+        && students.every((s) => typeof s === 'string')
+      ) {
+        this._students = students;
+      } else {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
   }
-
-  // get name
-  get name() {
-    return this._name;
-  }
-
-  // set name
-  set name(newName) {
-    this._name = newName;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  // set length
-  set length(newLength) {
-    this._length = newLength;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  // set students
-  set students(newStudents) {
-    this._students = newStudents;
-  }
-
-  // toString method for better representation
-  toString() {
-    return `${this.name}, ${this.length}, ${this.students}`;
-  }
-}
+  
+  export default HolbertonCourse;
