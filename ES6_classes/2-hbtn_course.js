3d7ba6ef7f3fe37106cf4243e929349393524b04
sonia <https://github.com/Soniabensaad>
2023-11-15 11:29:55 +0100

    ES promise

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name,
+    this._length = length,
+    this._students = students;
+  }
+
+  // get name
+  get name() {
+    return this._name;
+  }
+
+  // set length
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  // set length
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // set students
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+
+  course() {
+    console.log(`${this.name}, ${this.length}, ${this.students}`);
+  }
+}
