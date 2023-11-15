5a58e3443bace68ccf37f4715cfb595cec460370
sonia <https://github.com/Soniabensaad>
2023-11-15 18:00:44 +0100

    ES promise

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,22 @@
 export default class Building {
-    constructor(sqft){
-        this._sqft = sqft;
+    constructor(sqft) {
+      this._sqft = sqft;
     }
+  
     get sqft() {
-        return this._sqft;
+      return this._sqft;
+    }
+  
+    set sqft(value) {
+      if (typeof value === 'number') {
+        this._sqft = value;
       }
+    }
+  
+    static evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     
-      set sqft(value) {
-        if (typeof value === 'number') {
-          this._amount = value;
-        }
-      }
     
-}
+  }
+  
