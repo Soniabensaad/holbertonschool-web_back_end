ed707a2d51a08239ea8a342428b66acaa772b779
sonia <https://github.com/Soniabensaad>
2023-11-17 21:29:08 +0100

    ES promise

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+// car.js
+export class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this.brand, this.motor, this.color);
+  }
+}
