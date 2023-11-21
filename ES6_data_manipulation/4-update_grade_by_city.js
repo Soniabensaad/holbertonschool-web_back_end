c2293668d1d8a90f9edf5751e24ecb96aec32652
sonia <https://github.com/Soniabensaad>
2023-11-21 12:27:48 +0100

    data manipulation

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,14 @@
-const updateStudentGradeByCity = (students, city, newGrades) =>
-  students
-    .filter((obj) => obj.location === city)
-    .map((student) => {
-      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+const updateStudentGradeByCity = (students, city, newGrades) => students
+  .filter((obj) => obj.location === city)
+  .map((student) => {
+    const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
 
-      return {
-        id: student.id,
-        firstName: student.firstName,
-        location: student.location,
-        grade: matchingGrade ? matchingGrade.grade : 'N/A',
-      };
-    });
+    return {
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: matchingGrade ? matchingGrade.grade : 'N/A',
+    };
+  });
 
 export default updateStudentGradeByCity;
