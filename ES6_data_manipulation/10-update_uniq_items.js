1dd85d647a6a3c615914b66de9f4a196403eb27e
sonia <https://github.com/Soniabensaad>
2023-11-23 16:20:04 +0100

    data manipulation

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+const updateUniqueItems = (groceryMap) => {
+    if (!(groceryMap instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+  
+    groceryMap.forEach((quantity, item) => {
+      if (quantity === 1) {
+        groceryMap.set(item, 100);
+      }
+    });
+  };
+  
+  export default updateUniqueItems;
+  
