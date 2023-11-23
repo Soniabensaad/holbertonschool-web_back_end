fde625af2392926b92989af669badeace8a6f403
sonia <https://github.com/Soniabensaad>
2023-11-23 12:46:31 +0100

    data manipulation

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,22 +1,17 @@
 const cleanSet = (set, startString) => {
     const resultArray = [];
-  
+
     set.forEach((value) => {
-      if (typeof value === "string") {
-        const stringValue = String(value); // Convert non-string values to string
-        if (stringValue.startsWith(startString)) {
-          const restOfString = stringValue.slice(startString.length);
-          resultArray.push(restOfString);
+        if (typeof value === "string" || resultArray.length === 0) {
+            if (value.startsWith(startString)) {
+                const restOfString = value.slice(startString.length);
+                resultArray.push(restOfString);
+            }
         }
-      }
     });
-  
-    if (resultArray.length === 0) {
-      return "";
-    }
-  
+
+
     return resultArray.join("-");
-  };
-  
-  export default cleanSet;
-  
+};
+
+export default cleanSet;
