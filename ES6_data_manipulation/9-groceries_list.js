24f0274e840f01d5a8593218ac2f1aa1cb3813b5
sonia <https://github.com/Soniabensaad>
2023-11-23 13:47:58 +0100

    data manipulation

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,20 +1,19 @@
 const groceriesList = () => {
-    const items = [
-      { name: 'Apples', quantity: 10 },
-      { name: 'Tomatoes', quantity: 10 },
-      { name: 'Pasta', quantity: 1 },
-      { name: 'Rice', quantity: 1 },
-      { name: 'Banana', quantity: 5 },
-    ];
-  
-    const groceryMap = new Map();
-  
-    items.forEach(({ name, quantity }) => {
-      groceryMap.set(name, quantity);
-    });
-  
-    return groceryMap;
-  };
-  
-  export default groceriesList;
-  
+  const items = [
+    { name: 'Apples', quantity: 10 },
+    { name: 'Tomatoes', quantity: 10 },
+    { name: 'Pasta', quantity: 1 },
+    { name: 'Rice', quantity: 1 },
+    { name: 'Banana', quantity: 5 },
+  ];
+
+  const groceryMap = new Map();
+
+  items.forEach(({ name, quantity }) => {
+    groceryMap.set(name, quantity);
+  });
+
+  return groceryMap;
+};
+
+export default groceriesList;
