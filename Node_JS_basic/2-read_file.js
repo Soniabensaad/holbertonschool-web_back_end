de7ec83fc50dde4cde2456b4980f8e5182393891
sonia <https://github.com/Soniabensaad>
2023-11-26 16:20:50 +0100

    data manipulation

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
+const csv = require('csv-parser');
 
 // Define the countStudents function
 const countStudents = (filePath) => {
     try {
-        return readDatabaseSync(filePath);
+        const students = readDatabaseSync(filePath);
+        const totalStudents = students.length;
+
+        console.log(`Number of students: ${totalStudents}`);
+
+        // Log the number of students for each field
+        const fields = Object.keys(students[0]);
+
+        fields.forEach((field) => {
+            const studentsInField = students
+                .filter((student) => student[field])
+                .map((student) => student[field]);
+
+            console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
+        });
     } catch (error) {
         console.error(error.message);
         // Handle the error appropriately
@@ -11,24 +26,24 @@ const countStudents = (filePath) => {
 };
 
 const readDatabaseSync = (filePath) => {
+    const students = [];
+
     try {
-        // Read the file synchronously
-        const data = fs.readFileSync(filePath, 'utf8');
-        console.log('File content:', data);
+        // Read the CSV file using csv-parser
+        const fileStream = fs.createReadStream(filePath);
+        fileStream
+            .pipe(csv())
+            .on('data', (row) => {
+                // Assuming 'name' is one of the fields in your CSV
+                if (row.name) {
+                    students.push(row);
+                }
+            })
+            .on('end', () => {
+                // File reading is complete
+            });
 
-        // Parse the data (replace this with your actual parsing logic)
-        const parsedData = JSON.parse(data); // Assuming the data is in JSON format
-
-        // Log the number of students for each field
-        const fields = Object.keys(parsedData.students[0]); // Assuming all students have the same fields
-
-        fields.forEach((field) => {
-            const numberOfStudents = parsedData.students.filter((student) => student[field]).length;
-            console.log(`Number of students in ${field}: ${numberOfStudents}`);
-        });
-
-        // Process the data or return it as needed
-        return parsedData;
+        return students;
     } catch (error) {
         console.error('Error reading file:', error.message);
         // If the error is due to the file not being found, throw a custom error
@@ -39,3 +54,5 @@ const readDatabaseSync = (filePath) => {
         throw error;
     }
 };
+
+module.exports = countStudents;
