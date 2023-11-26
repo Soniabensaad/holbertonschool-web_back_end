2ffb18262cbdd32275c2f30590c05a8e47cbd4a8
sonia <https://github.com/Soniabensaad>
2023-11-26 14:37:26 +0100

    data manipulation

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,7 @@
 // 0-main.js
 
-function displayMessage (string) {
-    process.stdout.write(string);
-}
+function displayMessage(string) {
+    console.log(string);
+  }
+  
 module.exports = displayMessage;
