1dcbd0443fa09a17e8bd92b4f0ac2c303f6f1560
sonia <https://github.com/Soniabensaad>
2023-11-26 16:26:59 +0100

    data manipulation

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,58 +1,41 @@
 const fs = require('fs');
-const csv = require('csv-parser');
 
-// Define the countStudents function
-const countStudents = (filePath) => {
-    try {
-        const students = readDatabaseSync(filePath);
-        const totalStudents = students.length;
+function countStudents(path) {
+  let content;
 
-        console.log(`Number of students: ${totalStudents}`);
+  try {
+    content = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 
-        // Log the number of students for each field
-        const fields = Object.keys(students[0]);
+  content = content.toString().split('\n');
 
-        fields.forEach((field) => {
-            const studentsInField = students
-                .filter((student) => student[field])
-                .map((student) => student[field]);
+  let students = content.filter((item) => item);
 
-            console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
-        });
-    } catch (error) {
-        console.error(error.message);
-        // Handle the error appropriately
+  students = students.map((item) => item.split(','));
+
+  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
     }
-};
+  }
 
-const readDatabaseSync = (filePath) => {
-    const students = [];
+  delete fields.field;
 
-    try {
-        // Read the CSV file using csv-parser
-        const fileStream = fs.createReadStream(filePath);
-        fileStream
-            .pipe(csv())
-            .on('data', (row) => {
-                // Assuming 'name' is one of the fields in your CSV
-                if (row.name) {
-                    students.push(row);
-                }
-            })
-            .on('end', () => {
-                // File reading is complete
-            });
-
-        return students;
-    } catch (error) {
-        console.error('Error reading file:', error.message);
-        // If the error is due to the file not being found, throw a custom error
-        if (error.code === 'ENOENT') {
-            throw new Error('Cannot load the database. File not found.');
-        }
-        // Handle other errors appropriately
-        throw error;
-    }
-};
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
+    );
+  }
+}
 
 module.exports = countStudents;
