ab963032b6483818d6f114fa369eb9f358f41d5b
sonia <https://github.com/Soniabensaad>
2023-11-26 15:16:03 +0100

    data manipulation

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,20 @@
-console.log("Welcome to Holberton School, what is your name?" + "\n");
+console.log('Welcome to Holberton School, what is your name?\n');
 
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Prompt the user for their name
 rl.question('', (name) => {
   // Display the message with the entered name on a new line
-  console.log("Your name is: " + name + "\n");
+  console.log(`Your name is: ${name}\n`);
 
   // Close the readline interface
   rl.close();
 
   // Display the closing message
-  console.log("This important software is now closing" + "\n");
+  console.log('This important software is now closing\n');
 });
