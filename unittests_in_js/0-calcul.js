1ec2b289480b9042ec2b2ee81b36a9699e9f955f
sonia <https://github.com/Soniabensaad>
2023-11-27 14:03:34 +0100

    unitest

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,6 +1,9 @@
-module.exports = function calculateNumber (a, b=0) {
-    let roundedA = Number(a);
-    let roundedB = Number(b);
-    let sum = Math.round(roundedA) + Math.round(roundedB);
-    return sum;
-}
+module.exports = function calculateNumber(a, b = 0) {
+    const aNum = Number(a);
+    const bNum = Number(b);
+    
+    if (Number.isNaN(aNum) || Number.isNaN(bNum))
+      throw TypeError('Parameters must be numbers');
+    
+      return Math.round(aNum) + Math.round(bNum);
+    };
