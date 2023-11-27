68680414faf982d55082afad4d65bdd1c5b8ca3a
sonia <https://github.com/Soniabensaad>
2023-11-27 13:47:17 +0100

    unitest

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,4 +1,4 @@
-moduleeports = function calculateNumber (a, b) {
+module.exports = function calculateNumber (a, b) {
     let roundedA = Number(a);
     let roundedB = Number(b);
     let sum = Math.round(roundedA) + Math.round(roundedB);
