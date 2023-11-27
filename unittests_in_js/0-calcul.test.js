d79380b5d3eb715d8ce004ce50c71f8586cbea7a
sonia <https://github.com/Soniabensaad>
2023-11-27 13:49:45 +0100

    unitest

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,24 +1,42 @@
-// 0-calcul.test.js
 const assert = require('assert');
 const mocha = require('mocha');
-const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function () {
-  it('should round and sum two numbers', function () {
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return sum of integers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(1, -3), -2);
   });
 
-  it('should round and sum two numbers with decimals', function () {
+  it('should round floats', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should round and sum two decimal numbers', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should round and sum two decimal numbers (another case)', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(0.1, 0), 0);
+    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
   });
 
-  // Add more test cases as needed
+  it('should return the rounded number if only one is provided', () => {
+    assert.strictEqual(calculateNumber(2), 2);
+    assert.strictEqual(calculateNumber(2.7), 3);
+  });
+
+  it('should cast non-numbers into numbers', () => {
+    assert.strictEqual(calculateNumber(true, '3'), 4);
+    assert.strictEqual(calculateNumber(1, '3.7'), 5);
+    assert.strictEqual(calculateNumber('1.2', 3.7), 5);
+  });
+
+  it('should throw typeerror if either param cannot be coerced to a number', () => {
+    assert.throws(() => calculateNumber('hello'), {
+      name: 'TypeError',
+      message: 'Parameters must be numbers'
+    });
+    assert.throws(() => calculateNumber(1.2, 'dog'), {
+      name: 'TypeError',
+      message: 'Parameters must be numbers'
+    });
+  });
 });
