e578ee9b344021dba95301e75dbcf78ec44320ff
sonia <https://github.com/Soniabensaad>
2023-12-01 22:44:20 +0100

    node

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,29 +1,22 @@
 const express = require('express');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
 
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
 app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const output = 'This is the list of our students\n';
   try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
+    const students = await countStudents(process.argv[2]);
+    res.end(`This is the list of our students\n${students.join('\n')}`);
   } catch (error) {
-    res.send(`${msg}${error.message}`);
+    res.end(output + error.message);
   }
 });
 
-app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
 
 module.exports = app;
