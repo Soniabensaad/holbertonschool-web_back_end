c677123ba2135606dfec054fdb9fd95f43ac4ac2
sonia <https://github.com/Soniabensaad>
2023-12-01 22:40:05 +0100

    node

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,23 +1,36 @@
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  if (req.url === '/') {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
     res.write('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(process.argv[2]);
+      const students = await countStudents(DATABASE);
       res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   }
+  res.statusCode = 404;
   res.end();
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
