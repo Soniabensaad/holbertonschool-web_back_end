505c46eccab39470e9d2a158ea3fb0603624e269
sonia <https://github.com/Soniabensaad>
2023-12-01 22:30:02 +0100

    node

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,7 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const args = process.argv.slice(2);
 const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
@@ -17,20 +16,34 @@ const app = http.createServer(async (req, res) => {
   if (url === '/') {
     res.write('Hello Holberton School!');
   } else if (url === '/students') {
-    res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      res.write('This is the list of our students\n');
+
+      // Display the number of students
+      res.write(`Number of students: ${students.length}\n`);
+
+      // Display the number of students in CS
+      const csStudents = students.filter(student => student.field === 'CS');
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student.firstname).join(', ')}\n`);
+
+      
+      
+      const sweStudents = students.filter(student => student.field === 'SWE');
+      res.end(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student.firstname).join(', ')}`);
     } catch (error) {
       res.end(error.message);
     }
+    return; 
   }
+
+ 
   res.statusCode = 404;
   res.end();
 });
 
 app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
