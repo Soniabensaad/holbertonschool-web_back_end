b81d8e0b8899e229b1490b219f4b4e44f8fae094
sonia <https://github.com/Soniabensaad>
2023-12-08 19:35:53 +0100

    api

--- queuing_system_in_js/8-job.test.js
+++ queuing_system_in_js/8-job.test.js
@@ -24,40 +24,39 @@ describe('createPushNotificationsJobs', () => {
     queue.testMode.exit();
   });
 
-  it('display a error message if jobs is not an array passing Number', () => {
+  it('throws an error if jobs is not an array (Number)', () => {
     expect(() => {
       createPushNotificationsJobs(2, queue);
-    }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
+    }).to.throw(/Jobs is not an array/);
   });
 
-  it('display a error message if jobs is not an array passing Object', () => {
+  it('throws an error if jobs is not an array (Object)', () => {
     expect(() => {
       createPushNotificationsJobs({}, queue);
-    }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
+    }).to.throw(/Jobs is not an array/);
   });
 
-  it('display a error message if jobs is not an array passing String', () => {
+  it('throws an error if jobs is not an array (String)', () => {
     expect(() => {
       createPushNotificationsJobs('Hello', queue);
-    }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
+    }).to.throw(/Jobs is not an array/);
   });
 
-  it('should NOT display a error message if jobs is an array with empty array', () => {
+  it('does not throw an error if jobs is an empty array', () => {
     const ret = createPushNotificationsJobs([], queue);
     expect(ret).to.equal(undefined);
-    // expect(queue.testMode.job[0]);
   });
 
-  it('create two new jobs to the queue', () => {
+  it('adds two jobs to the queue with the correct types and data', () => {
     queue.createJob('myJob', { foo: 'bar' }).save();
     queue.createJob('anotherJob', { baz: 'bip' }).save();
+
     expect(queue.testMode.jobs.length).to.equal(2);
     expect(queue.testMode.jobs[0].type).to.equal('myJob');
     expect(queue.testMode.jobs[0].data).to.eql({ foo: 'bar' });
     expect(queue.testMode.jobs[1].type).to.equal('anotherJob');
     expect(queue.testMode.jobs[1].data).to.eql({ baz: 'bip' });
   });
+
+  
 });
