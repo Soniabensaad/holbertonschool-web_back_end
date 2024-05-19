cc885f714566b8ab70b994f3466b8e605d3115fb
sonia <https://github.com/Soniabensaad>
2024-05-19 17:33:49 +0200

    unitest

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,19 +1,30 @@
+// 1-calcul.jsfunction 
+function isNegZero(n) {
+    n = Number(n);
+    return n === 0 && 1 / n === -Infinity;
+  }
+  
+
 function calculateNumber(type, a, b) {
-  const aRound = Math.round(a);
-  const bRound = Math.round(b);
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+  
+    switch (type) {
+      case 'SUM':
+        return roundedA + roundedB;
+      case 'SUBTRACT':
+        return roundedA - roundedB;
+      case 'DIVIDE':
+        if (roundedB === 0) {
+          throw new Error('Cannot divide by zero');
+        }
+        const quotient = roundedA / roundedB;
+        return isNegZero(quotient) ? 0 : quotient;
 
-  if (type === 'SUBTRACT') {
-    return aRound - bRound;
-  }
-
-  if (type === 'DIVIDE') {
-    if (bRound === 0) {
-      return 'Error';
+      default:
+        throw new Error('Invalid operation type');
     }
-    return aRound / bRound;
   }
-
-  return aRound + bRound;
-}
-
-module.exports = calculateNumber;
+  
+  module.exports = calculateNumber;
+  
