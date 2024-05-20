e11475d4df6ad5fba1e8907c0d2173c9a511f65d
sonia <https://github.com/Soniabensaad>
2024-05-20 16:23:58 +0200

    unitest

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,83 +1,88 @@
 #!/usr/bin/env python3
-"""Tasks"""
-import redis
-from uuid import uuid4
-from typing import Union, Callable, Optional
+""" Redis module. """
+
 from functools import wraps
+import redis
+import sys
+from typing import Union, Optional, Callable
+from uuid import uuid4
 
 
-class Cache:
-    """Method"""
-    def __init__(self):
-        """self"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store"""
-        random = str(uuid4())
-        self._redis.set(random, data)
-        return random
-
-    def get(self, key: str, fn: Callable = None
-            ) -> Union[str, bytes, int, float]:
-        """get function"""
-        find = self._redis.get(key)
-        if find is not None:
-            if fn is not None:
-                return fn(find)
-            else:
-                return find
-        return None
-
-    def get_str(self, key: str) -> str:
-        """get str function"""
-        return self._redis.get(key).decode("utf-8")
-
-    def get_int(self, key: str) -> int:
-        """get int function"""
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode('utf-8'))
-        except Exception:
-            value = 0
-        return value
+def replay(method: Callable):
+    """ Replay. """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+    count = method.__self__.get(key)
+    i_list = method.__self__._redis.lrange(i, 0, -1)
+    o_list = method.__self__._redis.lrange(o, 0, -1)
+    queue = list(zip(i_list, o_list))
+    print(f"{key} was called {decode_utf8(count)} times:")
+    for k, v, in queue:
+        k = decode_utf8(k)
+        v = decode_utf8(v)
+        print(f"{key}(*{k}) -> {v}")
 
 
-def count_calls(method: Callable = None) -> Callable:
-    """count calls"""
-    name = method.__qualname__
-
+def call_history(method: Callable) -> Callable:
+    """ Call history. """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ wrapper function"""
-        self._redis.incr(name)
+        """ Wrapp """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count calls """
+    key = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapp """
+        self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
 
 
-def call_history(method: Callable) -> Callable:
-    """call hystory"""
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        input_str = str(args)
-        self._redis.rpush(method.__qualname__ + ":inputs", input_str)
-
-        output = str(method(self, *args, **kwargs))
-        self._redis.rpush(method.__qualname__ + ":outputs", output)
-
-        return output
-
-    return wrapper
+def decode_utf8(b: bytes) -> str:
+    """ Decodes """
+    return b.decode('utf-8') if type(b) == bytes else b
 
 
-def replay(self, method: Callable):
-    """replay"""
-    method_name = method.__qualname__
-    inputs = self._redis.lrange(f"{method_name}:inputs", 0, -1)
-    outputs = self._redis.lrange(f"{method_name}:outputs", 0, -1)
+class Cache:
+    """ Cache class. """
 
-    print(f"{method_name} was called {len(inputs)} times:")
-    for input_str, output in zip(inputs, outputs):
-        input_args = eval(input_str)
-        print(f"{method_name}({input_args}) -> {output}")
+    def __init__(self):
+        """ Init """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Random to store """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    bytes,
+                                                                    int,
+                                                                    float]:
+        """ Gets  """
+        res = self._redis.get(key)
+        return fn(res) if fn else res
+
+    def get_str(self, data: bytes) -> str:
+        """ Bytes to string """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Bytes to integer """
+        return int.from_bytes(data, sys.byteorder)
